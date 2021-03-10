15a405011df4875a3716ea552d7acaa0f26a8242
DoggySazHi <william.le.huy@gmail.com>
2021-03-10 13:48:44 -0800

    WOOO WE FINISHED 2011! (Finished FaxMachine.)

--- 2011/Flash Drive/Team #__ java files/FaxMachine.java
+++ 2011/Flash Drive/Team #__ java files/FaxMachine.java
@@ -14,30 +14,34 @@ public class FaxMachine
 {
    public static String convertToRLE(String str)
    {
-      var arr = Arrays.stream(str.split("1")).collect(Collectors.toList());
-      int index = str.length() - 1;
-      while (index >= 0 && str.charAt(index--) == '1')
-         arr.add("");
-      return arr.stream()
-              .map(String::length)
-              .map(o -> {
-                 boolean addZero = o % 7 == 0;
-                 var out = new ArrayList<Integer>();
-                 while (o > 0) {
-                    var val = Math.min(o, 7);
-                    o -= val;
-                    out.add(val);
-                 }
-                 if (addZero)
-                    out.add(0);
-                 return out;
-              })
-              .flatMap(List::stream)
-              .map(o ->
+      var counts = new ArrayList<Integer>();
+      var chars = str.toCharArray();
+      int accum = 0;
+      for (var c : chars) {
+          if (c == '0')
+              ++accum;
+          else {
+              counts.add(accum);
+              accum = 0;
+          }
+      }
+      counts.add(accum);
+
+       var sequence = new ArrayList<Integer>();
+       for(var c : counts) {
+           while (c >= 7) {
+               sequence.add(7);
+               c -= 7;
+           }
+           sequence.add(c);
+       }
+
+       return sequence.stream()
+               .map(o ->
                    ((o & 0b100) == 4 ? "1" : "0") +
                    ((o & 0b010) == 2 ? "1" : "0") +
                    ((o & 0b001) == 1 ? "1" : "0"))
-              .collect(Collectors.joining());
+               .collect(Collectors.joining());
    }
 
    public static String RLEtoFax(String str)
@@ -51,22 +55,25 @@ public class FaxMachine
                  (bin.charAt(2) == '1' ? 1 : 0);
          arr.add(num);
       }
-      /*
-      for(var i = 0; i < arr.size() - 1; ++i) {
-         if (arr.get(i + 1) != 0) {
-            arr.set(i, arr.get(i) + arr.get(i + 1));
-            arr.remove(i + 1);
-            --i;
-         } else if (arr.get(i) % 7 == 0){
-            arr.remove(i + 1);
-         }
-      }*/
-      var sb = new StringBuilder();
-      for(var i = 0; i < arr.size() - 1; ++i) {
-         sb.append("0".repeat(arr.get(i)));
-         sb.append("1");
+
+      var output = new ArrayList<Integer>();
+      var accum = 0;
+      for(var v : arr) {
+          accum += v;
+          if (v < 7) {
+              output.add(accum);
+              accum = 0;
+          }
       }
-      sb.append("0".repeat(arr.get(arr.size() - 1)));
+
+      var sb = new StringBuilder();
+
+      for(int i = 0; i < output.size() - 1; ++i) {
+          sb.append("0".repeat(output.get(i)));
+          sb.append("1");
+      }
+      sb.append("0".repeat(output.get(output.size() - 1)));
+
       return sb.toString();
    }
 }
