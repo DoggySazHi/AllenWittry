89b94d2df3fd0c3850d714de739bfdbee1269939
DoggySazHi <william.le.huy@gmail.com>
2021-04-20 12:07:39 -0700

    Did RPSSL because it was part of my comp sci final.

--- 2013/Student Flash Drive/team #/RPSSL.java
+++ 2013/Student Flash Drive/team #/RPSSL.java
@@ -1,15 +1,41 @@
 import java.util.*;
 
-public class RPSSL 
-{
-   public String playRound(Player p1, Player p2)
-   {
-      if (p1.getName().equals("Sheldon") && p2.getName().equals("Bernadette")) return "TIE";
-      if (p1.getName().equals("Sheldon") && p2.getName().equals("Howard")) return "Sheldon";
+public class RPSSL {
+   private static class Pair {
+      String source;
+      String beats;
 
-      if (p1.getName().equals("Raj") && p2.getName().equals("Penny")) return "Penny";
-      if (p1.getName().equals("Amy") && p2.getName().equals("Leonard")) return "Leonard";
+      Pair(String source, String beats) {
+         this.source = source;
+         this.beats = beats;
+      }
 
-   	  return "How did I get here";
+      boolean equals(String source, String beats) {
+         return this.source.equals(source) && this.beats.equals(beats);
+      }
+   }
+
+   private static final List<Pair> beatMap;
+
+   static {
+      beatMap = new ArrayList<>();
+      beatMap.add(new Pair("rock", "lizard"));
+      beatMap.add(new Pair("rock", "scissors"));
+      beatMap.add(new Pair("scissors", "lizard"));
+      beatMap.add(new Pair("scissors", "paper"));
+      beatMap.add(new Pair("lizard", "paper"));
+      beatMap.add(new Pair("lizard", "spock"));
+      beatMap.add(new Pair("spock", "rock"));
+      beatMap.add(new Pair("spock", "scissors"));
+      beatMap.add(new Pair("paper", "spock"));
+      beatMap.add(new Pair("paper", "rock"));
+   }
+
+   public String playRound(Player p1, Player p2) {
+      if (beatMap.stream().anyMatch(o -> o.equals(p1.getChoice(), p2.getChoice())))
+         return p1.getName();
+      if (beatMap.stream().anyMatch(o -> o.equals(p2.getChoice(), p1.getChoice())))
+         return p2.getName();
+      return "TIE";
    }
 }
